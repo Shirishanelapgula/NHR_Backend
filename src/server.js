84f4e458a58204b20a8189e355af91542196b4f7
shirishanelapogula <shirisha@godspeed.systems>
2024-01-28 14:10:09 +0530

    updated server.js

--- src/server.js
+++ src/server.js
@@ -15,9 +15,9 @@ const corsOptions = {
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
 
-  app.use(cors(corsOptions))
-
 app.use(express.json())
+
+app.use(cors(corsOptions))
 
 app.use("/test/state",stateRoute)
 app.use("/test/task",taskRouter)
